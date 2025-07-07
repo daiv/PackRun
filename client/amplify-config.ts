219c7a9ede16b94396acfa148a5cda8df8ec4f11
David-Torre-Ubuntu <5156172+daiv@users.noreply.github.com>
2025-07-07 06:52:39 +0200

    configure amplify

--- client/amplify-config.ts
+++ client/amplify-config.ts
@@ -5,8 +5,8 @@ const amplifyConfig = {
   Auth: {
 
     Cognito: {
-      userPoolId: "eu-north-1_RqnVqAHxb", // Your User Pool ID
-      userPoolClientId: "1a492i6mgl4vgj6n3ugs1c6v5q", // Your App Client ID
+      userPoolId: "eu-north-1_S29aYSOiO", // Your User Pool ID
+      userPoolClientId: "57c1iqk9ge04q7jfeodto9j3jr", // Your App Client ID
       region: "eu-north-1", // Your AWS Region 
     }
   }

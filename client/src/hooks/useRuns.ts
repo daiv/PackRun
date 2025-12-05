e4cc267b8e015491ede54c5cdf771dbb0642c4a0
David-Torre-Ubuntu <5156172+daiv@users.noreply.github.com>
2025-12-05 12:07:39 +0100

    fix: useRuns now updates runs on focus

--- client/src/hooks/useRuns.ts
+++ client/src/hooks/useRuns.ts
@@ -1,47 +1,45 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { useConnContext } from "@context";
 import { Run, RunResponse } from "../types/types";
+import { useFocusEffect } from "@react-navigation/native";
 
 export function useRuns() {
   const [runs, setRuns] = useState<Run[]>([]);
   const { fetchData } = useConnContext();
 
-  const getRuns = async (): Promise<RunResponse[]> => {
+  const getRuns = useCallback(async () => {
     const runsArray = await fetchData<RunResponse[]>('/tracks/', 'GET');
     console.log('response', runsArray);
     if (runsArray?.success === false) {
       console.error('Error getting runs from server', runsArray.error);
-      return [];
     } else {
-      return runsArray?.data ? runsArray.data : [];
+      if (runsArray && runsArray.data) {
+        console.log('nº runs', runsArray.data.length);
+        setRuns(
+          runsArray.data.map((run: RunResponse) => {
+            let seconds = (new Date(run.updatedAt).getTime() - new Date(run.createdAt).getTime()) / 1000;
+            const hours = Math.floor(seconds / 3600);
+            const minutes = Math.floor((seconds % 3600) / 60);
+            const remainingSeconds = Math.floor(seconds % 60);
+            const pad = (num: number) => String(num).padStart(2, '0');
+            const minPac = (hours * 60) + minutes;
+            const pace = String(Math.floor(Number(run.distance) / minPac));
+
+            return {
+              id: run.trackId,
+              date: new Date(run.createdAt).toDateString(),
+              time: `${pad(hours)}:${pad(minutes)}:${pad(remainingSeconds)}`,
+              pace,
+              distance: run.distance,
+              profile: run.altitudes
+            };
+          }).sort((a: Run, b: Run) => Number(a.id) - Number(b.id))
+        )
+      }
     }
-  }
+  }, [fetchData]);
 
-  useEffect(() => {
-    getRuns()
-      .then(runs => setRuns(
-        runs.map((run: RunResponse) => {
-          console.log('nº runs', runs.length);
-          let seconds = (new Date(run.updatedAt).getTime() - new Date(run.createdAt).getTime()) / 1000;
-          const hours = Math.floor(seconds / 3600);
-          const minutes = Math.floor((seconds % 3600) / 60);
-          const remainingSeconds = Math.floor(seconds % 60);
-          const pad = (num: number) => String(num).padStart(2, '0');
-          const minPac = (hours * 60) + minutes;
-          const pace = String(Math.floor(Number(run.distance) / minPac));
-
-          return {
-            id: run.trackId,
-            date: new Date(run.createdAt).toDateString(),
-            time: `${pad(hours)}:${pad(minutes)}:${pad(remainingSeconds)}`,
-            pace,
-            distance: run.distance,
-            profile: run.altitudes
-          };
-        }).sort((a: Run, b: Run) => Number(a.id) - Number(b.id))
-      ));
-    console.log('final runs', runs);
-  }, []);
+  useFocusEffect(useCallback(() => { getRuns() }, [getRuns]));
 
   const deleteRun = useCallback(async (runId: string) => {
     const response = await fetchData(`/tracks/${runId}`, 'DELETE', null);
